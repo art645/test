c9edc7db10569d334943878a7b80b2ee3569a16a
aefanov <aefanov95@inbox.ru>
2023-08-15 17:43:43 +0300

    last commit

--- src/main/java/Pes.java
+++ src/main/java/Pes.java
@@ -1,5 +1,7 @@
 public class Pes {
     public static void main(String[] args) {
         System.out.println("wow");
+        System.out.println("double ducks");
+        System.out.println("droid");
     }
 }
